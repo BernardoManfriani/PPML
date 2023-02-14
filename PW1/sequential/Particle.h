d0f8066c212994137ca599bb5374aa60092b89ad
BernardoManfriani <bernardomanfriani@gmail.com>
2023-02-14 19:12:46 +0100

    Created sub-dir

---
+++ PW1/sequential/Particle.h
@@ -0,0 +1,37 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+#include <vector>
+#include <iostream>
+#include <stdlib.h>	//for rand()
+#include <time.h>	//for time()
+#include "MazeGenerator.h"
+
+using namespace std;
+
+class Particle{
+    public : 
+        Particle(vector<int> particle, MazeGenerator maze);
+        //void printParticle(vector<int> particle);
+        void move(MazeGenerator maze);
+        vector<int> chooseDirection(int direction);
+        void moveRight();
+        void moveDown();
+        void moveLeft();
+        void moveUp();
+        vector<vector<int>> getPath();
+         //find path
+        
+        
+
+    private :
+        vector<int> particle;
+        vector<vector<int>> maze1;
+        vector<vector<int>> path; 
+        void findPath();
+        void solutionFind(int iteration, MazeGenerator maze);
+
+};
+
+
+#endif
---
+++ PW1/sequential/main.cpp
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h> //for time()
+#include<vector>
+#include<iostream>
+#include"MazeGenerator.h"
+#include"Particle.h"
+#include<unistd.h>
+
+using namespace std;
+
+//main function
+
+int main(){
+    vector<vector<int>> matrix(GRID_DIM, vector<int>(GRID_DIM, 0)); //matrix initialization
+    MazeGenerator maze1(matrix);   //maze initialization
+
+    vector<vector<int>> maze2 = maze1.getMaze(); //inserisco in una matrice il maze generato dalla classe
+ 
+    //maze1.printMaze(maze2, 0); //print maze
+    
+    //vector<int> particlePosition{1,1};   //particle initialization
+    Particle particle({1,1}, maze1);    //particle initialization
+
+    vector<vector<int>> path = particle.getPath();  //path initialization
+
+    //cout << path[0].size() << endl;
+    //print path
+    /*
+    for(int i = 0; i < path.size(); i++){
+            cout << "(" << path[i][0] << "," << path[i][1] << ")" << endl;
+        //cout << endl;
+    }
+    */
+    
+    //print maze with path
+    for(int i = 0; i < path.size(); i++){
+        maze2[path[i][0]][path[i][1]] = 2;
+    }
+    
+    //maze1.printMaze(maze2, 0);
+    
+    
+    //per ogni punto senza vicini elimino il punto 
+    for(int i = 0; i < GRID_DIM - 1; i++){
+        for(int j = 0 ; j < GRID_DIM - 1; j++){
+            if (i > 0 && j > 0 && maze2[i][j] == 2){
+                if((maze2[i + 1][j] == 0 || maze2[i + 1][j] == 1) && (maze2[i - 1][j] == 0 || maze2[i - 1][j] == 1) && (maze2[i][j + 1] == 0 || maze2[i][j + 1] == 1) && (maze2[i][j - 1] == 0 || maze2[i][j - 1] == 1)){
+                        maze2[i][j] = 0;
+                }
+            }
+        }
+    }
+    
+
+    //maze1.printMaze(maze2, 0);
+
+
+    return 0;
+    
+}
